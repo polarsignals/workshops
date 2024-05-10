3764bd463fd2a249f72803b9b4de52fb1a1dd745
Matthias Loibl <mail@matthiasloibl.com>
2024-05-10 13:40:04 +0100

    Add C and Rust workshop examples

--- c/main.c
+++ c/main.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
-int main() {
-   printf("profiling\n");
-   return 0;
+#include <math.h>
+#include <stdbool.h>
+
+bool is_prime(int n) {
+    if (n <= 1) return false;
+    // TODO: Not use sqrt but i*i < n to be inline with other languages
+    for (int i = 2; i <= sqrt(n); i++) {
+        if (n % i == 0) return false;
+    }
+    return true;
 }
 
+void find_primes(int limit) {
+    for (int num = 2; num <= limit; num++) {
+        if (is_prime(num)) {
+            printf("%d\n", num);
+        }
+    }
+}
+
+int main() {
+    int limit = 1000000;
+    find_primes(limit);
+    return 0;
+}
--- rust/main.rs
+++ rust/main.rs
@@ -1,3 +1,28 @@
+use std::f64;
+
+fn is_prime(n: i32) -> bool {
+    if n <= 1 {
+        return false;
+    }
+    let limit = (n as f64).sqrt() as i32;
+    for i in 2..=limit {
+        if n % i == 0 {
+            return false;
+        }
+    }
+    true
+}
+
+fn find_primes(limit: i32) {
+    for num in 2..=limit {
+        if is_prime(num) {
+            println!("{}", num);
+        }
+    }
+}
+
 fn main() {
-  println!("profiling")
+    // TODO: Wrap with endless loop and 1s sleep
+    let limit = 1_000_000;
+    find_primes(limit);
 }
