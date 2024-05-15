d3eb71de3507f81fbed3262ef9b6cd976573a339
David Andersson <51036209+jdkandersson@users.noreply.github.com>
2024-05-15 10:22:03 +0200

    Update main.py

--- python/1/main.py
+++ python/1/main.py
@@ -1,14 +1,16 @@
 import time
 import unittest
 
-def findPrimes(limit = 10_000):
+
+def find_primes(limit = 10_000):
 	primes = []
 	for i in range(2, limit):
-		if isPrime(i):
+		if is_rime(i):
 			primes.append(i)
 	return primes
 
-def isPrime(n):
+
+def is_prime(n):
 	if n <= 1:
 		return False
 	for i in range(2, n):
@@ -22,17 +24,20 @@ def isPrime(n):
 if __name__ == '__main__':
 	while True:
 		start = time.time()
-		count = len(findPrimes(1_000_000))
+		count = len(find_primes(1_000_000))
 		end = time.time()
-		print("%d primes in %fs found" % (count, end - start))
+		print(f"{count} primes in {end - start} found")
 		time.sleep(1)
+
 
 class PrimeTest(unittest.TestCase):
 	def test_primes(self):
-		self.assertEqual([2, 3, 5, 7], findPrimes(10))
+		self.assertEqual([2, 3, 5, 7], find_primes(10))
+
 
 if __name__ == '__unittest__':
 	unittest.main()
 
+
 def test_my_stuff(benchmark):
-	benchmark(findPrimes)
+	benchmark(find_primes)
